3cb2586bbb5a03b2e29df10b864c63da8959a173
Praveen <geeks4share@gmail.com>
2019-01-25 19:41:13 +0530

    fix

--- main.go
+++ main.go
@@ -1,79 +1,78 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/praveen001/ds/list/arraylist"
-)
-
 func main() {
-	al := arraylist.New()
+	// al := arraylist.New()
 
-	al.Append(1, 2, 3, 4, 5, 6)
-	fmt.Println(al)
-
-	al.Prepend(0, -1)
-	fmt.Println(al)
-
-	fmt.Println(al.Filter(func(x interface{}) bool {
-		if x.(int)%2 != 0 {
-			return true
-		}
-		return false
-	}))
-
-	al.Remove(0)
-	fmt.Println(al)
-
-	al.Set(0, 10)
-	fmt.Println(al)
-
-	tal := al.Map(func(x interface{}) interface{} {
-		return x.(int) * 2
-	})
-
-	fmt.Println(al.Concat(tal))
-
-	fmt.Println(al.Contains(6), al.IndexOf(1))
-
-	cl := al.Clone()
-	cl.Clear()
-	fmt.Println(cl, al)
-
-	// fmt.Println(al.Reverse())
-	// for i := 0; i < 100; i++ {
-	// 	al.Append(i)
-	// }
-	// al.Append(10, 20, 30, 40, 50)
-	// fmt.Println(al.Values(), len(al.Values()))
-	// fmt.Println("Append 10, 20, 30, 40, 50 =>", al)
-
-	// al.Prepend(-10, 0, 5)
-	// fmt.Println("Prepend -10, 0 =>", al)
-
-	// // fmt.Println(al.Get(0), al.Get(-1), al.Get(6), al.Get(10))
-
-	// al.Set(0, -20)
-	// al.Set(6, 100)
+	// al.Append(1, 2, 3, 4, 5, 6)
 	// fmt.Println(al)
 
-	// fmt.Println("Removing")
-	// fmt.Println(al.Remove(-1))
-	// fmt.Println(al.Remove(0))
-	// fmt.Println(al.Remove(6))
-	// fmt.Println(al.Remove(3))
-	// fmt.Println(al.Remove(10))
+	// al.Prepend(0, -1)
 	// fmt.Println(al)
 
-	// fmt.Println(al.Contains(40), al.Contains(100))
-	// fmt.Println(al.IndexOf(40), al.IndexOf(100), al.Size())
+	// fmt.Println(al.Filter(func(x interface{}) bool {
+	// 	if x.(int)%2 != 0 {
+	// 		return true
+	// 	}
+	// 	return false
+	// }))
 
-	// var l list.List
+	// al.Remove(0)
+	// fmt.Println(al)
+
+	// al.Set(0, 10)
+	// fmt.Println(al)
+
+	// tal := al.Map(func(x interface{}) interface{} {
+	// 	return x.(int) * 2
+	// })
+
+	// fmt.Println(al.Concat(tal))
+
+	// fmt.Println(al.Contains(6), al.IndexOf(1))
+
+	// cl := al.Clone()
+	// cl.Clear()
+	// fmt.Println(cl, al)
+
+	// ------------------------
 
 	// s := stack.New()
-	// s.Push(10)
+	// fmt.Println(s)
 
-	// fmt.Println(s.Pop())
+	// s.Push(10)
+	// s.Push(20)
+	// fmt.Println(s)
+
+	// fmt.Println(s.Peek())
+
+	// s.Pop()
+	// fmt.Println(s)
+
+	// fmt.Println(s.Length())
+
+	// q.Clear()
+	// fmt.Println(q)
+
+	// ----------------------------
+
+	// q := queue.New()
+	// fmt.Println(q)
+
+	// q.Enqueue(10)
+	// q.Enqueue(20)
+	// fmt.Println(q)
+
+	// fmt.Println(q.Peek())
+
+	// q.Dequeue()
+	// fmt.Println(q)
+
+	// fmt.Println(q.Length())
+
+	// q.Clear()
+	// fmt.Println(q)
+
+	// ---------------------------
 
 	// bt := binarytree.New(func(a, b interface{}) int {
 	// 	at := a.(int)
@@ -128,4 +127,5 @@ func main() {
 
 	// bt.Delete(5)
 	// fmt.Println(bt.InOrder())
+
 }
